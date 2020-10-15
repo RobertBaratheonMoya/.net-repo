660c0c3688eb1f07e83c231e935b4556f6d5cffe
Roberto Moya <roberto.moya_23@hotmail.com>
2020-10-15 03:35:46 -0500

    Adding local db
    Adding migration and local db

--- WebApiDePrueba/Startup.cs
+++ WebApiDePrueba/Startup.cs
@@ -27,7 +27,7 @@ namespace WebApiDePrueba
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
