5c5895a2cb1c1d09b29687cf9f47db9753237817
RobertBaratheonMoya <[roberto.moya_23@hotmail.com]>
2020-10-15 12:54:01 -0500

    Adding getters & a Post

---
+++ WebApiDePrueba/Controllers/AutoresController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using WebApiDePrueba.Contexts;
+using WebApiDePrueba.Entities;
+
+namespace WebApiDePrueba.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.ToList();
+        }
+
+        [HttpGet("{id}", Name ="GetAutor")]
+        public ActionResult<Autor> Get(int id)
+        {
+            var autor = context.Autores.FirstOrDefault(X500DistinguishedName => X500DistinguishedName.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Autor autor)
+        {
+            context.Autores.Add(autor);
+            context.SaveChanges();
+            return new CreatedAtRouteResult("GetAutor", new { id = autor.Id }, autor);
+        }
+    }
+}
